4dd9a60193f8f785e6c969eeb86e2a9b42bbd034
kbskdev <milosz.olsztyn@gmail.com>
2023-02-16 10:55:48 +0100

    getting image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {HttpService} from "./service/http.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private api:HttpService) {
+  }
+  ngOnInit() {
+    this.api.getImage().subscribe(data=>{
+      this.imageFromBlob(data)
+    })
+  }
+  image:any
+
+  imageFromBlob(image:Blob){
+    let reader = new FileReader()
+    reader.addEventListener("load", () => {
+      this.image = reader.result;
+    }, false);
+
+    if (image) {
+      reader.readAsDataURL(image);
+    }
+  }
+
   title = 'pinboardfrontend';
 }
