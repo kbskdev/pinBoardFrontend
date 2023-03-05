a7542aa5b0d329cdd5997f5a304cbf82d9d31094
kbskdev <milosz.olsztyn@gmail.com>
2023-03-06 00:31:57 +0100

    .com added

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -10,10 +10,10 @@ export class AuthService {
   constructor(private http:HttpClient) { }
 
   login(email:string,password:string):Observable<any>{
-    return this.http.post('http://kbskdev/api/v1/users/login/',{email:email,password:password})
+    return this.http.post('http://kbskdev.com/api/v1/users/login/',{email:email,password:password})
   }
 
   register(username:string,email:string,password:string):Observable<any>{
-    return this.http.post('http://kbskdev/api/v1/users/register/',{username:username,email:email,password:password})
+    return this.http.post('http://kbskdev.com/api/v1/users/register/',{username:username,email:email,password:password})
   }
 }
